798b5efaa5d12d1833185de008c6f88d3c6331c9
MascaraChico <MascaraChico@users.noreply.github.com>
2017-03-14 22:33:20 +0500

    hahahahahh! See You Face!

---
+++ server.js
@@ -0,0 +1,13 @@
+// grab express
+var express = require('express');
+// create an express app
+var app = express();
+// create an express route for the home page
+// http://localhost:8080/
+app.get('/', function(req, res) {
+res.sendFile(__dirname +'/decent.html');
+});
+// start the server on port 8080
+app.listen(8080);
+// send a message
+console.log('Server has started!');
